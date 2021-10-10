b5e6c7427b47eef0f9a318723d5553e2e60a3765
Ashley Saleem-west <BooleanWhale@users.noreply.github.com>
2021-10-10 17:07:15 +0100

    index-page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,34 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/Layout'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Home({pokemons}) {
 
-  const getPokeImageUrl = (pokemonName) => `https://www.pokemon.com/us/pokedex/${pokemonName}`; 
+  console.log(pokemons)
 
   return (
     <Layout title={'Pokedex Home'}>
 
       <main>
         <h1 className="text-4xl mb-8 text-center">Pokedex with Next.js & Tailwind</h1>
+        <div>
+          <ul>
+            { pokemons.map(pokemon => {
+              return <li key={pokemon.number}>
+                <Link href={`/pokemon?id=${pokemon.number}`}>
+                  <a title={pokemon.name} className="border p-4 border-gray my-2 flex items-center text-lg bg-gray-200 rounded-md">
+                    <img src={pokemon.image} alt={pokemon.name} className="w-20 h-20 mr-3"/>
+                    <div>
+                      <span className="mr-2 font-bold">#{pokemon.number}</span>
+                      <span>{pokemon.name}</span>
+                    </div>
+                  </a>
+                </Link>
+              </li>
+            })}
+          </ul>
+        </div>
       </main>
 
     </Layout>
@@ -21,8 +39,17 @@ export default function Home() {
 export async function getStaticProps(context) {
 
   try {
-    const result = fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
-    const {results} = await result.json()
+    const data = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+    const {results} = await data.json()
+    const pokemons = results.map((result, index) => {
+      const number = ('00' + (index + 1)).slice(-3);
+      const name = result.name[0].toUpperCase() + result.name.slice(1);
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${number}.png`
+      return {...result, number, image, name}
+    })
+    return {
+      props: {pokemons}
+    }
   } catch (error) {
     console.error(error);
   }
